8ab464ddcd706d8925e70e06c06aa9b7d6853e0f
Khiari Samar <khiarisamaar13@gmail.com>
2022-01-27 10:48:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Im1 from '../src/175889183_264785515373475_7273384263359610096_n.jpg'
+export default class App extends Component {
+  constructor(props){
+    super(props)
+  }
+    state = {
+      person: {
+        fullname:'Khiari Samar',
+        bio:'I have passion with web development',
+        imgSrc: Im1,
+        Profession:'Student'
+      },
+      show:true,
+      timer:0
+    };
+    showProfile= () =>{
+     this.setState({...this.state,show: !this.state.show})
+    }
+    componentDidMount(){
+     setInterval(()=>this.setState({timer:this.state.timer+1}),1000)
+    }
+    componentDidUpdate(){
+      console.log('componentDidUpdate')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    }
+    componentWillUnmount(){
+      console.log('componentWillUnmount')
+    }
+  render() {
+    console.log('rendering')
+    return (
+      <div>
+      {this.state.show && (
+
+      <div >
+        <h1 style={{ display:'flex',justifyContent:'center'   }}> {this.state.person.fullname}  </h1>
+        <h2 style={{ display:'flex',justifyContent:'center'   }}>  {this.state.person.bio}  </h2>
+        <h3 style={{ display:'flex',justifyContent:'center'   }}>  {this.state.person.Profession} </h3>
+        <img src={this.state.person.imgSrc} alt='Photo' style={{ display:'flex',justifyContent:'center',alignItems:'center'}}></img>
+<p>{this.state.timer}</p>
+      </div>
+    )}
+    <button onClick={()=>this.showProfile({show:this.state.show})}>Person Profile | Hide Profile</button>
     </div>
+  
   );
+      }
 }
-
-export default App;
